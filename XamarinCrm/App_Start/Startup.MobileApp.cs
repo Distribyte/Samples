4e8fbdb7ef1609ebd329b9976237a659a2fca390
Gilad Eisenberger <gilad@distribyte.net>
2016-12-01 15:44:15 +0200

    Initial commit of Xamarin CRM sample code

---
+++ XamarinCrm/App_Start/Startup.MobileApp.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Owin;
+using System.Web.Http;
+
+namespace XamarinCRMAppService
+{
+    public partial class Startup
+    {
+        public static void ConfigureMobileApp(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+#if DEBUG
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+#endif
+
+            app.UseWebApi(config);
+        }
+    }
+}
+
---
+++ XamarinCrm/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Http;
+using XamarinCRMAppService.Models;
+
+namespace XamarinCRMAppService.Controllers
+{
+    public abstract class BaseController : ApiController
+    {
+        public static MobileServiceContext Context;
+    }
+}
